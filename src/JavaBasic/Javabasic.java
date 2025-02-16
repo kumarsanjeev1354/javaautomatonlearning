900d7e61c965e370f52143ffe877e5013d308d3a
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2025-02-16 14:59:50 +0530

    Operators are added

--- src/JavaBasic/Javabasic.java
+++ src/JavaBasic/Javabasic.java
@@ -5,7 +5,6 @@ public class Javabasic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
-System.out.println("Hello World2");
 	}
 
 }
---
+++ src/JavaBasic/Operators.java
@@ -0,0 +1,21 @@
+package JavaBasic;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Arthematic Operators =,-,/,*,%
+		int x=50,y=30;
+		int z=x+y;
+	System.out.println(z);
+	System.out.println(x+y);
+	System.out.println(50+30);
+	System.out.println("Differnence of x and y is"+" " +(x-y));
+	System.out.println("Product of x and y is"+ " " +(x*y));
+	System.out.println("Division of x and y is"+ " "+ (x/y));// Need to check this
+	System.out.println("Modular of x and y is"+" "+ (x%y));
+//	Relational or Comparison Operator
+	
+	}
+
+}
