491143b515a16a210f53273c2f5d89c1023299a5
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2025-02-16 17:46:22 +0530

    Operators Completed till Increment and Decrement Operator

--- src/JavaBasic/Javabasic.java
+++ src/JavaBasic/Javabasic.java
@@ -4,7 +4,6 @@ public class Javabasic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World");
-	}
-
+System.out.println("Hello World");//This is print statement
+		}
 }
