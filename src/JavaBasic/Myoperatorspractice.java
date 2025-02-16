1130b65eec447f50b63ec77f1060c08b3617895f
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2025-02-17 00:36:32 +0530

    Added pre decrement operator

--- src/JavaBasic/Myoperatorspractice.java
+++ src/JavaBasic/Myoperatorspractice.java
@@ -6,8 +6,11 @@ public class Myoperatorspractice {
 		// TODO Auto-generated method stub
 int x=50;
 int y=x++;
-System.out.println(y);
-System.out.println(x);//updated
+System.out.println(y);//Value of y would be 50 as value of x will be used first then will be incremented
+System.out.println(x);//updated 51
+int z=--x;
+System.out.println(z);//50
+System.out.println(x);//50
 	}
 
 }
